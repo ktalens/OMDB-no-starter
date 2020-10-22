62950ff5b5fecb48b02166b4eb3f3babf7fe6465
ktalens <ktal66@yahoo.com>
2020-10-21 21:34:11 -0700

    search results have links that will take the user to the show route that displays the details for each search result

--- index.js
+++ index.js
@@ -22,14 +22,70 @@ app.get('/movies',(req,res)=>{
     axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.API_KEY.toString()}&s=${movieSearch}`)
     .then(response=>{
         //res.send(response.data)
-        let movieData=response.data.Search
-        //res.send(movieData[0].Title)
-        res.render('movies',{movieData: movieData})
+        let searchResultKeys = Object.keys(response.data)
+        if (searchResultKeys.includes('Search')) {
+            let movieData=response.data.Search
+            res.render('movies',{searchResults: movieData})
+        } else if (searchResultKeys.includes('Error')) {
+            let noResults= [
+                { Title: 'There were no results for that search',
+                imdbID: 'no results to display'
+                }
+            ]
+            res.render('movies',{searchResults: noResults })
+        }
+        
     })
     .catch(err=>{
         console.log(err)
     })
 })
+
+    // -----> RAW DATA FOR TITANIC <-----
+app.get('/data',(req,res)=>{
+    let movieSearch = req.query.q
+     axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.API_KEY.toString()}&s=Titanic`)
+    .then(response=>{
+        res.send(response.data)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+    // -----> RAW DATA FOR TITANIC WARS <-----
+app.get('/data2',(req,res)=>{
+    let movieSearch = req.query.q
+     axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.API_KEY.toString()}&s=Titanic+Wars`)
+    .then(response=>{
+        res.send(response.data)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+ // -----> SHOW ROUTE <-----
+app.get('/movies/:idx',(req,res)=>{
+    //res.send(req.params.idx)
+    axios.get(`http://www.omdbapi.com/?i=${req.params.idx}&apikey=${process.env.API_KEY.toString()}`)
+    .then(response=>{
+        let OMDbMovieData = response.data
+        let dataKeys= Object.keys(OMDbMovieData)
+        let dataValues= Object.values(OMDbMovieData)
+        let x = {dataKeys: dataValues}
+        res.render('show',{detailKeys: dataKeys, detailValues: dataValues})
+        //res.render('show',{OMDbMovieData: movieData})
+        //res.send(response.data)
+
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+    //res.render('show')
+})
+
+
 
 app.listen(process.env.PORT,()=>{
     console.log('OMDB is running...')
